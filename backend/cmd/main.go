3ad9e97bd91182ebfb9dd1d5e1bd779edad3adef
khailequang334 <khaihsgs@gmail.com>
2024-08-01 23:23:26 +0700

    Basic handling functions in BE

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,68 @@
+// main.go
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+type Message struct {
+	Sender string `json:"sender"`
+	Text   string `json:"text"`
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error while upgrading connection:", err)
+		return
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println("Error reading JSON:", err)
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for msg := range broadcast {
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println("Error sending message:", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	fmt.Println("WebSocket server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/khailequang334/chat_app
+
+go 1.21.6
+
+require github.com/gorilla/websocket v1.5.3
